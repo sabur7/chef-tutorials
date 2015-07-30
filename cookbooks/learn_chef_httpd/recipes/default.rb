762456986f620ba5861938d7ff2b659f0f17b41c
root <root@C2445575914.domain>
2015-07-30 15:47:01 -0400

    created and ran first cookbook successfully

---
+++ cookbooks/learn_chef_httpd/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: learn_chef_httpd
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
+
+service 'iptables' do
+  action :stop
+end
---
+++ webserver.rb
@@ -0,0 +1,18 @@
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>Hello World</h1>
+  </body>
+</html>'
+end
+
+service 'iptables' do
+  action :stop
+end
+
